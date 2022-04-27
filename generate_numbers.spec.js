b32537d5c3b3745b7461651e72cac50c31c44d7c
Jay <jaydeshmukh8551@gmail.com>
2022-04-27 15:12:24 +0530

    minor changes

--- generate_numbers.spec.js
+++
@@ -1,7 +0,0 @@
-const generateNumbers = require('./generate_numbers');
-
-it('generate numbers from 1 to 5', () => {
-  expect(generateNumbers(1,5)).toEqual([
-    1,2,3,4,5
-  ])
-});
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
+const generateNumbers = require('./generate_numbers');
+
 describe("FizzBuzz", () => {
-  it("initializes", () => {
-    expect(true).toBe(true);
+
+  it('generate numbers from 1 to 5', () => {
+    expect(generateNumbers(1,5)).toEqual([
+      1,2,3,4,5
+    ])
   });
 });
